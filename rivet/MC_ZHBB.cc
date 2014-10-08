cb9cdfddf829855e3230a496ad481aa215d2087b
Philipp Eller <peller@phys.ethz.ch>
2014-10-08 13:46:17 +0200

    update

--- rivet/MC_ZHBB.cc
+++ rivet/MC_ZHBB.cc
@@ -4,7 +4,7 @@
 #include "Rivet/Analyses/MC_JetAnalysis.hh"
 #include "Rivet/Projections/ZFinder.hh"
 #include "Rivet/Projections/FastJets.hh"
-/// @todo Include more projections as required, e.g. ChargedFinalState, FastJets, ZFinder...
+#include "Rivet/Projections/VetoedFinalState.hh"
 
 namespace Rivet {
 
@@ -18,10 +18,6 @@ namespace Rivet {
       : Analysis("MC_ZHBB")
     {    
     }
-
-
-    /// @name Analysis methods
-    //@{
 
     /// Book histograms and initialise projections before the run
     void init() {
@@ -38,102 +34,129 @@ namespace Rivet {
         // find Zee
         ZFinder zeefinder(fs, cut, PID::ELECTRON, 65*GeV, 115*GeV, 0.2);
         addProjection(zeefinder, "zeefinder");
+        // fish out Zee
+        VetoedFinalState zmminput;
+        zmminput.addVetoOnThisFinalState(zeefinder);
         // find Zmm
-        ZFinder zmmfinder(zeefinder.remainingFinalState(), cut, PID::MUON, 65*GeV, 115*GeV, 0.2);
+        ZFinder zmmfinder(zmminput, cut, PID::MUON, 65*GeV, 115*GeV, 0.2);
         addProjection(zmmfinder, "zmmfinder");
+        // fish out Zee and Zmm
+        VetoedFinalState jetproinput;
+        jetproinput.addVetoOnThisFinalState(zeefinder);
+        jetproinput.addVetoOnThisFinalState(zmmfinder);
         // make jets
-        FastJets jets(zmmfinder.remainingFinalState(), FastJets::ANTIKT, 0.5);
-        addProjection(jets, "jets");
+        FastJets jetpro(jetproinput, FastJets::ANTIKT, 0.5);
+        addProjection(jetpro, "jetpro");
+
 
         // ---- HISTOS ----
+        int nbins =100;
 
         // jets
         for (size_t i=0; i < njets; ++i) {
             string dname = "log10_d_" +to_str(i) + to_str(i+1);
-            _h_log10_d.push_back(bookHisto1D(dname, 100, 0.2, 4));
+            _h_log10_d.push_back(bookHisto1D(dname, nbins, 0.2, 4));
             dname = "aJet_pT" +to_str(i);
-            _h_ajets_pT.push_back(bookHisto1D(dname,logspace(100,5,300)));
+            _h_ajets_pT.push_back(bookHisto1D(dname,logspace(nbins,5,300)));
         }
 
-
         // Z properties
-        _h_Z_mass = bookHisto1D("Z_mass",80,51,131);
-        _h_Z_pT = bookHisto1D("Z_pT",logspace(100,5,300));
+        _h_Z_mass = bookHisto1D("Z_mass",nbins,51,131);
+        _h_Z_eta = bookHisto1D("Z_eta",nbins,-5,5);
+        _h_Z_pT = bookHisto1D("Z_pT",logspace(nbins,5,300));
 
         // dijet properties
-        _h_dijet_mass = bookHisto1D("dijet_mass",80,60,140);
-        _h_dijet_pT = bookHisto1D("dijet_pT",logspace(100,5,300));
+        _h_dijet_mass = bookHisto1D("dijet_mass",nbins,60,140);
+        _h_dijet_eta = bookHisto1D("dijet_eta",nbins,-5,5);
+        _h_dijet_pT = bookHisto1D("dijet_pT",logspace(nbins,5,300));
+
+        // higgs jets
+        for (size_t i=0; i < 2; ++i) {
+            string dname = "h_dau_mass" + to_str(i);
+            _h_dau_mass.push_back(bookHisto1D(dname,nbins,0,160));
+            dname = "h_dau_eta" + to_str(i);
+            _h_dau_eta.push_back(bookHisto1D(dname,nbins,-5,5));
+            dname = "h_dau_pT" + to_str(i);
+            _h_dau_pT.push_back(bookHisto1D(dname,logspace(nbins,5,300)));
+        }
 
     }
-
 
     /// Perform the per-event analysis
     void analyze(const Event& event) {
         const double weight = event.weight();
 
-        float jets_pt_cut = 0.0*GeV;
+        double jets_pt_cut = 5.0*GeV;
 
+        // apply the projections
         const ZFinder& zeefinder = applyProjection<ZFinder>(event, "zeefinder");
         const ZFinder& zmmfinder = applyProjection<ZFinder>(event, "zmmfinder");
         const Particles zll = zeefinder.bosons() + zmmfinder.bosons();
-        const FastJets& jets = applyProjection<FastJets>(event,"jets");
-        const Jets& alljets = jets.jetsByPt(jets_pt_cut);
+        const FastJets& jetpro = applyProjection<FastJets>(event,"jetpro");
+        const Jets alljets = jetpro.jetsByPt();
        
         // only continue if a Z has been found
         if (zll.empty()) vetoEvent;
 
-        Jets bjets;
         Jets ajets;
+        ajets.clear();
         Jet h0, h1; 
         FourMomentum  dijet;
+        std::vector<size_t> b_indices;
 
-        foreach (const Jet& jet, alljets) {
-            if (jet.bTagged()) bjets.push_back(jet);
-            else ajets.push_back(jet);
+        // find all jets with bs
+        for (size_t i = 0; i < alljets.size(); ++i) {
+            if (alljets[i].containsBottom())  b_indices.push_back(i);
             }
 
-        if (bjets.size() < 2) vetoEvent;
+        // require at least to b jets
+        if (b_indices.size() < 2) vetoEvent;
 
         // construct highest dijet pt b-jet pair
         double max_pt = 0.0*GeV;
         size_t idx0 = 0;
         size_t idx1 = 0;
-        for (size_t i = 0; i < bjets.size()-1; ++i) {
-            for (size_t j = i+1; j < bjets.size(); ++j) {
-                dijet = bjets[i].momentum() + bjets[j].momentum();
+        // pairwise iterate
+        for (size_t i = 0; i < b_indices.size()-1; ++i) {
+            for (size_t j = i+1; j < b_indices.size(); ++j) {
+                dijet = alljets[b_indices[i]].momentum() + alljets[b_indices[j]].momentum();
                 if (dijet.pT() > max_pt) {
                     max_pt = dijet.pT();
-                    idx0 = i;
-                    idx1 = j;
+                    idx0 = b_indices[i];
+                    idx1 = b_indices[j];
                     }
                 }
             }
 
-        // add everything else to the ajets 
-        for (size_t i = 0; i < bjets.size(); ++i) {
-            // for now identify the first two b-jets as higgs
-            if (i == idx0) h0 = bjets[i];
-            if (i == idx1) h1 = bjets[i];
-            if (i != idx0 && i != idx1) ajets.push_back(bjets[i]);
-            }
-
+        h0 = alljets[idx0];
+        h1 = alljets[idx1];
         // final highest pt dijet pair
         dijet = h0.momentum() + h1.momentum();
-
-        // sort additional jets
-        //ajets = ajets.jetsByPt(jets_pt_cut)
+        
+        // add everything else to the ajets 
+        for (size_t i = 0; i < alljets.size(); ++i) {
+            if (i != idx0 && i != idx1) ajets.push_back(alljets[i]);
+            }
 
         // fill histos
+        _h_dau_mass[0]->fill(h0.mass(),weight);
+        _h_dau_mass[1]->fill(h1.mass(),weight);
+        _h_dau_eta[0]->fill(h0.eta(),weight);
+        _h_dau_eta[1]->fill(h1.eta(),weight);
+        _h_dau_pT[0]->fill(h0.pT(),weight);
+        _h_dau_pT[1]->fill(h1.pT(),weight);
+
         _h_Z_mass->fill(zll[0].mass(),weight);
+        _h_Z_eta->fill(zll[0].eta(),weight);
         _h_Z_pT->fill(zll[0].pT(),weight);
 
         _h_dijet_mass->fill(dijet.mass(),weight);
+        _h_dijet_eta->fill(dijet.eta(),weight);
         _h_dijet_pT->fill(dijet.pT(),weight);
-
 
         // Jet resolutions and integrated jet rates
         double d_ij;
-        const fastjet::ClusterSequence* seq = jets.clusterSeq();
+        const fastjet::ClusterSequence* seq = jetpro.clusterSeq();
         if (seq != NULL) {
             for (size_t i = 0; i < njets; ++i) {
                  // Jet resolution i -> j
@@ -146,7 +169,6 @@ namespace Rivet {
                 _h_ajets_pT[i]->fill(ajets[i].pT(),weight);
             }
         }
-
     }
 
 
@@ -158,43 +180,37 @@ namespace Rivet {
             scale(_h_log10_d[i], crossSection()/sumOfWeights());
             scale(_h_ajets_pT[i], crossSection()/sumOfWeights());
         }
-        scale(_h_Z_mass, crossSection()/sumOfWeights());
-        scale(_h_dijet_mass, crossSection()/sumOfWeights());
-        scale(_h_Z_pT, crossSection()/sumOfWeights());
-        scale(_h_dijet_pT, crossSection()/sumOfWeights());
 
+        for (size_t i = 0; i < 2; ++i) {
+            scale(_h_dau_mass[i], crossSection()/sumOfWeights());
+            scale(_h_dau_eta[i], crossSection()/sumOfWeights());
+            scale(_h_dau_pT[i], crossSection()/sumOfWeights());
+        }
+
+        scale(_h_Z_mass, crossSection()/sumOfWeights());
+        scale(_h_Z_eta, crossSection()/sumOfWeights());
+        scale(_h_Z_pT, crossSection()/sumOfWeights());
+        scale(_h_dijet_mass, crossSection()/sumOfWeights());
+        scale(_h_dijet_eta, crossSection()/sumOfWeights());
+        scale(_h_dijet_pT, crossSection()/sumOfWeights());
 
         // scale(_h_YYYY, crossSection()/sumOfWeights()); // norm to cross section
         // normalize(_h_YYYY); // normalize to unity
-      
-        // Scale the d{eta,phi,R} histograms
 
     }
 
-    //@}
-
-
   private:
 
-    // Data members like post-cuts event weight counters go here
-
-
-    /// @name Histograms
-    //@{
     size_t njets;
-    //Profile1DPtr _h_XXXX;
-    //Histo1DPtr _h_deta_jets, _h_dphi_jets, _h_dR_jets;
     std::vector<Histo1DPtr> _h_log10_d;
     std::vector<Histo1DPtr> _h_ajets_pT;
-    Histo1DPtr _h_Z_mass, _h_Z_pT, _h_dijet_pT, _h_dijet_mass;
-    //@}
-
+    std::vector<Histo1DPtr> _h_dau_mass;
+    std::vector<Histo1DPtr> _h_dau_eta;
+    std::vector<Histo1DPtr> _h_dau_pT;
+    Histo1DPtr _h_Z_mass, _h_Z_pT, _h_Z_eta, _h_dijet_pT, _h_dijet_mass, _h_dijet_eta;
 
   };
 
-
-
-  // The hook for the plugin system
   DECLARE_RIVET_PLUGIN(MC_ZHBB);
 
 }
--- rivet/MC_ZHBB.plot
+++ rivet/MC_ZHBB.plot
@@ -8,14 +8,21 @@
 # ... add more histograms as you need them ...
 #
 # BEGIN PLOT /MC_ZHBB/Z_mass
-Title=Z mass
+Title=mass of Z-Bososn
 XLabel=$M_Z$ (GeV)
 YLabel=$\text{d}\sigma/\text{d}M_Z$ (pb/GeV)
 LogY=1
 # END PLOT
+#
+# BEGIN PLOT /MC_ZHBB/Z_eta
+Title=psudorapidity of Z-Bososn
+XLabel=$\eta(Z)$ (GeV)
+YLabel=$\text{d}\sigma/\text{d}\eta(Z)$ (pb)
+LogY=1
+# END PLOT
 
 # BEGIN PLOT /MC_ZHBB/Z_pT
-Title=Z $p_{T}$
+Title=transverse momentum of Z-Bososn
 XLabel=$p_{T}(Z)$ (GeV)
 YLabel=$\text{d}\sigma/\text{d}p_{T}(Z)$ (pb/GeV)
 LogY=1
@@ -23,47 +30,63 @@ LogX=1
 # END PLOT
 
 # BEGIN PLOT /MC_ZHBB/dijet_mass
-Title=di-jet mass
+Title=mass of di-jet
 XLabel=$M_{jj}$ (GeV)
 YLabel=$\text{d}\sigma/\text{d}M_{jj}$ (pb/GeV)
 LogY=0
 # END PLOT
 
 # BEGIN PLOT /MC_ZHBB/dijet_pT
-Title=di-jet $p_{T}$
+Title=transverse momentum of di-jet
 XLabel=$p_{T}(jj)$ (GeV)
 YLabel=$\text{d}\sigma/\text{d}p_{T}(jj)$ (pb/GeV)
 LogY=1
 LogX=1
 # END PLOT
 
+# BEGIN PLOT /MC_ZHBB/h_dau_pT0
+Title=transverse momentum of leading higgs jet
+XLabel=$p_{T}(h_0)$ (GeV)
+YLabel=$\text{d}\sigma/\text{d}p_{T}(h_0)$ (pb/GeV)
+LogY=1
+LogX=1
+# END PLOT
+
+# BEGIN PLOT /MC_ZHBB/h_dau_pT1
+Title=transverse momentum of second higgs jet
+XLabel=$p_{T}(h_1)$ (GeV)
+YLabel=$\text{d}\sigma/\text{d}p_{T}(h_1)$ (pb/GeV)
+LogY=1
+LogX=1
+# END PLOT
+
 # BEGIN PLOT /MC_ZHBB/aJet_pT0
-Title=leading jet $p_{T}$
-XLabel=$p_{T}(jet0)$ (GeV)
+Title=transverse momentum of leading jet
+XLabel=$p_{T}(jet_0)$ (GeV)
 YLabel=$\text{d}\sigma/\text{d}p_{T}(jet0)$ (pb/GeV)
 LogY=1
 LogX=1
 # END PLOT
 
 # BEGIN PLOT /MC_ZHBB/aJet_pT1
-Title=second jet $p_{T}$
-XLabel=$p_{T}(jet1)$ (GeV)
+Title=transverse momentum of second jet
+XLabel=$p_{T}(jet_1)$ (GeV)
 YLabel=$\text{d}\sigma/\text{d}p_{T}(jet1)$ (pb/GeV)
 LogY=1
 LogX=1
 # END PLOT
 
 # BEGIN PLOT /MC_ZHBB/aJet_pT2
-Title=third jet $p_{T}$
-XLabel=$p_{T}(jet2)$ (GeV)
+Title=transverse momentum of third jet
+XLabel=$p_{T}(jet_2)$ (GeV)
 YLabel=$\text{d}\sigma/\text{d}p_{T}(jet2)$ (pb/GeV)
 LogY=1
 LogX=1
 # END PLOT
 
 # BEGIN PLOT /MC_ZHBB/aJet_pT3
-Title=fourth jet $p_{T}$
-XLabel=$p_{T}(jet3)$ (GeV)
+Title=transverse momentum of fourth jet
+XLabel=$p_{T}(jet_3)$ (GeV)
 YLabel=$\text{d}\sigma/\text{d}p_{T}(jet3)$ (pb/GeV)
 LogY=1
 LogX=1
@@ -73,30 +96,22 @@ LogX=1
 Title=$\log_{10}$($k_\perp$ jet resolution $0 \to 1$ (GeV))
 XLabel=$\log_{10}(d_{01}/\text{GeV})$
 YLabel=$\text{d}\sigma/\text{d}\log_{10}(d_{01}/\text{GeV})$ (pb)
-LegendXPos=0.15
-LegendYPos=0.5
 # END PLOT
 
 # BEGIN PLOT /MC_ZHBB/log10_d_12
 Title=$\log_{10}$($k_\perp$ jet resolution $1 \to 2$ (GeV))
 XLabel=$\log_{10}(d_{12}/\text{GeV})$
 YLabel=$\text{d}\sigma/\text{d}\log_{10}(d_{12}/\text{GeV})$ (pb)
-LegendXPos=0.15
-LegendYPos=0.5
 # END PLOT
 
 # BEGIN PLOT /MC_ZHBB/log10_d_23
 Title=$\log_{10}$($k_\perp$ jet resolution $2 \to 3$ (GeV))
 XLabel=$\log_{10}(d_{23}/\text{GeV})$
 YLabel=$\text{d}\sigma/\text{d}\log_{10}(d_{23}/\text{GeV})$ (pb)
-LegendXPos=0.15
-LegendYPos=0.5
 # END PLOT
 
 # BEGIN PLOT /MC_ZHBB/log10_d_34
 Title=$\log_{10}$($k_\perp$ jet resolution $3 \to 4$ (GeV))
 XLabel=$\log_{10}(d_{34}/\text{GeV})$
 YLabel=$\text{d}\sigma/\text{d}\log_{10}(d_{34}/\text{GeV})$ (pb)
-LegendXPos=0.15
-LegendYPos=0.5
 # END PLOT
