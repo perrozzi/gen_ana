eb71c3b86ee388306c86092ab5bbf2b2853113c6
Philipp Eller <peller@phys.ethz.ch>
2014-12-04 16:47:10 +0100

    adding data

--- rivet_plots/make_plots.sh
+++ rivet_plots/make_plots.sh
@@ -3,7 +3,7 @@ XSEC=0.0583
 rivet-mkhtml \
 powheg_HZ_inc.aida:Scale=$XSEC:"Title:Powheg HZ + Pythia8" \
 powheg_HZJ_inc.aida:Scale=$XSEC:"Title:Powheg HZJ + Pythia8" \
-aMC_pythia_50_norm.aida:Scale=$XSEC:"Title:aMC@NLO 0,1j + Pyhtia8" \
+amc_pythia_inc.aida:Scale=$XSEC:"Title:aMC@NLO 0,1j + Pyhtia8" \
 aMC_pythia_0j_norm.aida:Scale=$XSEC:"Title:aMC@NLO 0j + Pyhtia8" \
 ZH_merged_norm.yoda:Scale=$XSECBR:"Title:aMC@NLO 0,1,2j + Herwig6" \
 ZH_inclusive_norm.yoda:Scale=$XSECBR:"Title:aMC@NLO 0j + Herwig6" \
@@ -14,21 +14,28 @@ Sherpa_LOPS.yoda:Scale=$XSECBR:"Title:Sherpa LO+PS" \
 rivet-mkhtml --mc-errs -c config.plot -n 16 \
 powheg_HZ_inc.aida:Scale=$XSEC:ErrorBandColor=red:"Title:Powheg HZ + Pythia8" \
 powheg_HZJ_inc.aida:Scale=$XSEC:ErrorBandColor=red:"Title:Powheg HZJ + Pythia8" \
-aMC_pythia_50_norm.aida:Scale=$XSEC:ErrorBandColor=red:"Title:aMC@NLO 0,1j + Pyhtia8" \
-ZH_merged_norm.yoda:Scale=$XSECBR:ErrorBandColor=red:"Title:aMC@NLO 0,1,2j + Herwig6" \
+amc_pythia_inc.aida:Scale=$XSEC:ErrorBandColor=red:"Title:aMC@NLO 0,1j + Pyhtia8" \
+Sherpa_multijet_inc.yoda:Scale=0.00352:"Title:Sherpa-MC@NLO multijet" \
 Sherpa_Hll_inc.yoda:Scale=0.08:"Title:Sherpa-MC@NLO 0j" \
 -o inc
 rivet-mkhtml \
 powheg_HZ_sel.aida:Scale=$XSEC:"Title:Powheg HZ + Pythia8" \
 powheg_HZJ_sel.aida:Scale=$XSEC:"Title:Powheg HZJ + Pythia8" \
-aMC_pythia_50_sel.aida:Scale=$XSEC:"Title:aMC@NLO 0,1j + Pyhtia8" \
+amc_pythia_sel.aida:Scale=$XSEC:"Title:aMC@NLO 0,1j + Pyhtia8" \
+Sherpa_multijet_sel.yoda:Scale=0.00352:"Title:Sherpa-MC@NLO multijet" \
+Sherpa_Hll_sel.yoda:Scale=0.0022:"Title:Sherpa-MC@NLO 0j" \
+--mc-errs -c sel.plot -n 16 -o sel
+rivet-mkhtml \
+powheg_HZ_sel.aida:Scale=$XSEC:"Title:Powheg HZ + Pythia8" \
+powheg_HZJ_sel.aida:Scale=$XSEC:"Title:Powheg HZJ + Pythia8" \
+amc_pythia_sel.aida:Scale=$XSEC:"Title:aMC@NLO 0,1j + Pyhtia8" \
 aMC_pythia_0j_sel.aida:Scale=$XSEC:"Title:aMC@NLO 0j + Pyhtia8" \
 ZH_merged_sel.yoda:Scale=$XSECBR:"Title:aMC@NLO 0,1,2j + Herwig6" \
 ZH_inclusive_sel.yoda:Scale=$XSECBR:"Title:aMC@NLO 0j + Herwig6" \
-Sherpa_Hll_sel.yoda:Scale=$XSECBR:"Title:Sherpa-MC@NLO 0j" \
+Sherpa_Hll_sel.yoda:Scale=0.0022:"Title:Sherpa-MC@NLO 0j" \
 Sherpa_LOPS_sel.yoda:Scale=$XSECBR:"Title:Sherpa LO+PS" \
 Sherpa_012j_sel.yoda:Scale=$XSECBR:"Title:Sherpa-MC@NLO 0,1,2j" \
---mc-errs -c sel.plot -n 16 -o sel
+--mc-errs -c sel.plot -n 16 -o selall
 #rivet-mkhtml \
 #Sherpa_norm.yoda:"Title:Sherpa-MC@NLO 0j" \
 #Sherpa_LOPS.yoda:"Title:Sherpa LO+PS" \
