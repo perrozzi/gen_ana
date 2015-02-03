18dcd2accd392b62431b69a763bd42ea0b9c4ea9
Philipp Eller <peller@phys.ethz.ch>
2015-02-03 09:29:11 +0100

    up

---
+++ 8TeV/Paladini.plot
@@ -0,0 +1,32 @@
+# BEGIN PLOT /Paladini/*
+XLabel=$p_T(Z)$ (GeV)
+YLabel=$d\sigma/dp_T$
+# END PLOT
+
+# BEGIN PLOT /Paladini/d01-x01-y01
+Title=$n_{jet} > 1$
+# END PLOT
+
+# BEGIN PLOT /Paladini/d01-x02-y02
+Title=$n_{jet} > 2$
+# END PLOT
+
+# BEGIN PLOT /Paladini/d01-x03-y03
+Title=$n_{jet} > 3$
+# END PLOT
+
+# BEGIN PLOT /Paladini/d01-x04-y04
+Title=$H_T > 300 GeV$
+# END PLOT
+
+# BEGIN PLOT /Paladini/d01-x05-y05
+Title=$n_{jet} > 1,\ |y(Z)| < 1.4$
+# END PLOT
+
+# BEGIN PLOT /Paladini/d01-x06-y06
+Title=$n_{jet} > 2,\ |y(Z)| < 1.4$
+# END PLOT
+
+# BEGIN PLOT /Paladini/d01-x07-y07
+Title=$n_{jet} > 3,\ |y(Z)| < 1.4$
+# END PLOT
--- 8TeV/make_plots.sh
+++ 8TeV/make_plots.sh
@@ -1,15 +1,28 @@
 rivet-mkhtml -n 16 -c ATLAS_2014_I1279489.plot -o NLO \
 ATLAS_2014_I1279489.yoda \
-amc.yoda:NormalizeToIntegral=1:ErrorBands=1:ErrorBandOpacity=0.2:ErrorBandColor=red:"Title=aMC@NLO + Pythia8"
+AMC_py8_resubmit.yoda:NormalizeToIntegral=1:ErrorBands=1:ErrorBandOpacity=0.2:ErrorBandColor=red:"Title=aMC@NLO + Pythia8"
+
 rivet-mkhtml -n 16 -c ATLAS_2014_I1279489.plot -o LO \
-ATLAS_2014_I1279489.yoda \
-LO.yoda:NormalizeToIntegral=1:ErrorBands=1:ErrorBandOpacity=0.2:LineColor=blue:ErrorBandColor=blue:"Title=Madgraph + Pythia8"
+ATLAS_2014_I1279489.yoda :NormalizeToIntegral=1:ErrorBands=1:ErrorBandOpacity=0.2:LineColor=blue:ErrorBandColor=blue:"Title=Madgraph + Pythia8"
+MG_py8_resubmit.yoda:NormalizeToIntegral=1:ErrorBands=1:ErrorBandOpacity=0.2:LineColor=blue:ErrorBandColor=blue:"Title=Madgraph + Pythia8"
+
 rivet-mkhtml -n 16 -c ATLAS_2014_I1279489.plot -o Powheg \
 ATLAS_2014_I1279489.yoda \
-powheg_old.yoda:NormalizeToIntegral=1:ErrorBands=1:ErrorBandOpacity=0.2:LineColor=blue:ErrorBandColor=blue:"Title=Powheg + Pythia6"
+Powheg_resubmit.yoda:NormalizeToIntegral=1:ErrorBands=1:ErrorBandOpacity=0.2:LineColor=blue:ErrorBandColor=blue:"Title=Powheg + Pythia6"
+
 rivet-mkhtml -n 16 -c ATLAS_2014_I1279489.plot -o MG \
 ATLAS_2014_I1279489.yoda \
-adgraph_old.yoda:NormalizeToIntegral=1:ErrorBands=1:ErrorBandOpacity=0.2:LineColor=blue:ErrorBandColor=blue:"Title=Madgraph + Pythia6"
-rivet-mkhtml -n 16 -c bla -o MG-amc \
-madgraph_old.yoda:NormalizeToIntegral=1:ErrorBands=1:ErrorBandOpacity=0.2:LineColor=blue:ErrorBandColor=blue:"Title=Madgraph + Pythia6" \
-amc.yoda:NormalizeToIntegral=1:ErrorBands=1:ErrorBandOpacity=0.2:ErrorBandColor=red:"Title=aMC@NLO + Pythia8"
+madgraph_old.yoda:NormalizeToIntegral=1:ErrorBands=1:ErrorBandOpacity=0.2:LineColor=blue:ErrorBandColor=blue:"Title=Madgraph + Pythia6"
+
+rivet-mkhtml -n 16 -o all \
+AMC_py8_resubmit.yoda:NormalizeToIntegral=1:ErrorBands=1:ErrorBandOpacity=0.2:ErrorBandColor=red:"Title=aMC@NLO + Pythia8" \
+MG_py8_resubmit.yoda:NormalizeToIntegral=1:ErrorBands=1:ErrorBandOpacity=0.2:LineColor=blue:ErrorBandColor=blue:"Title=Madgraph + Pythia8" \
+madgraph_old.yoda:NormalizeToIntegral=1:ErrorBands=1:ErrorBandOpacity=0.2:LineColor=green:ErrorBandColor=green:"Title=Madgraph + Pythia6" \
+powheg_old.yoda:NormalizeToIntegral=1:ErrorBands=1:ErrorBandOpacity=0.2:LineColor=orange:ErrorBandColor=orange:"Title=Powheg + Pythia6"
+
+rivet-mkhtml -n 16 -c Paladini.plot -o paladini \
+Paladini.yoda \
+AMC_py8_resubmit.yoda:Scale=0.0000086:ErrorBands=1:ErrorBandOpacity=0.2:ErrorBandColor=red:"Title=aMC@NLO + Pythia8" \
+MG_py8_resubmit.yoda:Scale=0.082:ErrorBands=1:ErrorBandOpacity=0.2:LineColor=blue:ErrorBandColor=blue:"Title=Madgraph + Pythia8" \
+MG_py6_resubmit.yoda:Scale=0.065:ErrorBands=1:ErrorBandOpacity=0.2:LineColor=green:ErrorBandColor=green:"Title=Madgraph + Pythia6" \
+Powheg_resubmit.yoda:Scale=0.056:ErrorBands=1:ErrorBandOpacity=0.2:LineColor=orange:ErrorBandColor=orange:"Title=Powheg + Pythia6"
