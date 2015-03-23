fececb5501041db38b952d8a9ade499d4fda6e41
Philipp Eller <peller@phys.ethz.ch>
2015-03-23 12:17:35 +0100

    sherpa data

--- Zjets/make_plots.sh
+++ Zjets/make_plots.sh
@@ -1,9 +1,9 @@
 rivet-mkhtml -n 16 -c plot.conf -o ATLAS \
 ATLAS_2013_I1230812.yoda \
-powheg/ATLAS.yoda:Scale=0.0097:ErrorBandColor=red:"Title=Powheg + Pythia8" \
-NLO_5f/ATLAS.yoda:Scale=0.022:ErrorBandColor=blue:"Title=aMC@NLO + Pythia8" \
-LO_5f/ATLAS.yoda:Scale=0.014:ErrorBandColor=green:"Title=MG + Pythia8" \
-Sherpa_ATLAS.yoda:Scale=0.06:ErrorBandColor=orange:"Title=Sherpa MEPS@NLO"
+powheg/ATLAS.yoda:Scale=0.0098:ErrorBandColor=red:"Title=Powheg + Pythia8" \
+NLO_5f/ATLAS.yoda:Scale=0.02175:ErrorBandColor=blue:"Title=aMC@NLO + Pythia8" \
+LO_5f/ATLAS.yoda:Scale=0.0137:ErrorBandColor=green:"Title=MG + Pythia8" \
+Sherpa_ATLAS.yoda:Scale=0.0121:ErrorBandColor=orange:"Title=Sherpa MEPS@NLO"
 #LO_5f/ATLASold.yoda:Scale=0.014:ErrorBandColor=orange:"Title=MG + Pythia8 old"
 
 rivet-mkhtml -n 16 -c CMS_2013_I1258128.plot -o CMS2 \
@@ -26,7 +26,3 @@ Sherpa_CMS3.yoda:NormalizeToIntegral=1:ErrorBandColor=orange:"Title=Sherpa MEPS@
 #CMS_SMP_12_017.yoda \
 #LO_5f/CMS1.yoda:Scale=1.27:ErrorBandColor=green:"Title=MG + Pythia8"
 #NLO_5f/CMS1.yoda:Scale=1.27:ErrorBandColor=blue:"Title=aMC@NLO + Pythia8" \
-
-#rivet-mkhtml -n 16 -c plot.conf -o MC \
-#LO_5f/MC.yoda:Scale=1.27:ErrorBandColor=green:"Title=MG + Pythia8"
-#NLO_5f/MC.yoda:Scale=1.27:ErrorBandColor=blue:"Title=aMC@NLO + Pythia8" \
